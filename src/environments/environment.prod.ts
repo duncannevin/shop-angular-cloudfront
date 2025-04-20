2441e0f67a32436f345ba7ffbb1df7f9ce8d90cf
duncannevin <duncan.nevin@gmail.com>
2025-04-19 22:57:41 -0700

    feat: use products endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,14 +3,14 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://ql1arll5s0.execute-api.us-west-2.amazonaws.com/prod',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: false,
+    product: true,
     order: false,
     import: false,
     bff: false,
